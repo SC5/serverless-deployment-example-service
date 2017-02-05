59c52e1244185180ec0229979036cfe2a891c270
Eetu Tuomala <eetu.tuomala@sc5.io>
2017-02-05 23:53:25 +0200

    add ansible build plugin for complex deployment

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
 # Example Serverless project for deployment
 
-- Serverless with Ansible
-- Serverless with AWS Pipeline and AWS CodeBuild
+## Serverless Deployment with Ansible (Lite)
+[serverless-deployment-ansible-lite](https://github.com/SC5/serverless-deployment-ansible-lite) builds and deploys project with Serverless so no extra configuration is needed.
+
+## Serverless Deployment with Ansible
+[serverless-deployment-ansible](https://github.com/SC5/serverless-deployment-ansible) deploys already built serverless package.
+
+Here is the deployment flow in short:
+
+1. Build artifact with `sls deploy --ansible` ([serverless-ansible-build-plugin](https://github.com/laardee/serverless-ansible-build-plugin)) which creates Jinja2 template from CloudFormation stack and copies .zip file to deployment folder.
+2. Copy .j2 template and zip to Artifactory/S3 or where ever you wish.
+3. Ansible playbook downloads template and deploys CloudFormation stack and functions zip to AWS.
+
+More detailled instructions will be available in [serverless-deployment-ansible](https://github.com/SC5/serverless-deployment-ansible).
---
+++ scripts/create-artifact.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+
+# Defaults
+CREATE_PACKAGE=0
+DIRECTORY=".ansible"
+VERSION=1
+BUILDNO=1
+
+if ( ! getopts ":s:" opt); then
+	echo "option -s service-name required";
+	exit $E_OPTERROR;
+fi
+
+while getopts ":ps:d:v:b:" opt; do
+  case $opt in
+    p)
+      CREATE_PACKAGE=1 >&2
+      ;;
+    s)
+      SERVICE=$OPTARG >&2
+      ;;
+    d)
+      DIRECTORY=$OPTARG >&2
+      ;;
+    v)
+      VERSION=$OPTARG >&2
+      ;;
+    b)
+      BUILDNO=$OPTARG >&2
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+  esac
+done
+
+# Artifact
+HASH=$(git rev-parse --short HEAD)
+DATE=`date +%Y%m%d`
+TAR_NAME="$SERVICE-$VERSION.$DATE.$BUILDNO-$HASH.tar.gz"
+
+# Build
+docker run -v $(pwd):/src -w /src laardee/serverless:latest /bin/bash -c "\
+npm install && \
+if [ -d "$DIRECTORY" ]; then rm $DIRECTORY/* ; fi && \
+sls deploy --ansible && \
+[[ $CREATE_PACKAGE -eq true ]] || \
+{
+  tar -zcf $DIRECTORY/$TAR_NAME $DIRECTORY/$SERVICE.zip $DIRECTORY/$SERVICE.json.j2 ; \
+  echo Serverless Ansible Build Plugin: $DIRECTORY/$TAR_NAME created. ;
+}"
+
+[[ $? -eq 0 ]] || { echo "Build failed!" ; exit 1; }
